cc300e2ef9a86acf1d806fe1f2de75fb7331d16b
Brian Merchant <bhmerchant@gmail.com>
2023-11-02 14:40:34 -0700

    Create default-dirs.nix

---
+++ os/base/default-dirs.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+
+{
+  environment.etc."xdg/user-dirs.defaults".text = ''
+    CONFIG_DIRS=$HOME/config-sys
+    CONFIG_HOME=$HOME/config
+    CACHE_HOME=$HOME/cache
+    DATA_HOME=$HOME/local/share
+    STATE_HOME=$HOME/local/state
+    DESKTOP=$HOME
+    DOWNLOAD=$HOME/downloads
+    TEMPLATES=$HOME/templates
+    PUBLICSHARE=$HOME/public
+    DOCUMENTS=$HOME
+    MUSIC=$HOME/media
+    PICTURES=$HOME/media
+    VIDEOS=$HOME/media
+  '';
+}
