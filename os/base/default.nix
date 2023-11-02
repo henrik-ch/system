f2be620cd938f048a2c9c9b98df45498adcb2511
Brian Merchant <bhmerchant@gmail.com>
2023-11-01 22:08:05 -0700

    Remove boot settings from base/default.nix, so that they can be placed in host/book.nix

--- os/base/default.nix
+++ os/base/default.nix
@@ -7,18 +7,6 @@
     ./time.nix
     ./security.nix
   ];
-    
-  boot = {
-    initrd.systemd.enable = true;
-    loader = {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
-    };
-
-    # explicitly set which device is to be used for storing hibernation/sleep
-    # info if not ram
-    resumeDevice = "/dev/disk/by-label/SWAP";
-  };
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
