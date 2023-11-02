bd1284e315b7d301a36d33898a1c1c3b35d1e71a
Brian Merchant <bhmerchant@gmail.com>
2023-11-02 14:25:41 -0700

    we are using unstable, which is currently marked as 23.11

--- os/base/default.nix
+++ os/base/default.nix
@@ -32,5 +32,5 @@
     udisks2.enable = true;
  };
 
-  system.stateVersion = "23.05"; # Did you read the comment?
+  system.stateVersion = "23.11"; # Apparently, no need to change, in order to make it robust to syntax issues...
 }
