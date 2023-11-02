4950bdbfcb5d74bccdef3324469226a683d1de44
Brian Merchant <bhmerchant@gmail.com>
2023-11-02 16:05:24 -0700

    Create programs.nix

---
+++ os/programs.nix
@@ -0,0 +1,54 @@
+{ pkgs, ... }:
+
+{
+  programs = {
+    git = {
+      enable = true;
+      lfs.enable = true;
+      config = {
+        init = {
+          defaultBranch = "main";
+        };
+        user = {
+          name = "Brian Merchant";
+          email = "bhmerchant@gmail.com";
+        };
+        commit = {
+          gpgSign = true;
+        };
+        core = {
+          editor = "hx";
+        };
+        credential = {
+          helper = "manager";
+          credentialStore = "gpg";
+        };
+      };
+    };
+  };
+
+  users = {
+    defaultUserShell = "${pkgs.zsh}/bin/zsh";
+
+    users.bzm3r = {
+      isNormalUser = true;
+      home = "/home/bzm3r";
+      extraGroups = [ "wheel" "networkmanager" ];
+      useDefaultShell = true;
+
+      packages = with pkgs; [
+        nil
+        vscode
+        discord
+        github-desktop
+        gh
+        wakatime
+        git-credential-manager
+        pass
+
+        tealdeer
+        systeroid
+      ];
+    };
+  };
+}
