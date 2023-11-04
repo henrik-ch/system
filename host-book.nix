d89933de853651c96f98ed1b8d053d9fae8a9d8b
Brian Merchant <bzm3r@proton.me>
2023-11-04 12:25:53 -0700

    keep partition definition blocks together for now for clarity

--- host-book.nix
+++ host-book.nix
@@ -59,6 +59,11 @@
         fsType = "vfat";
     };
 
+  swapDevices =
+    [
+        { device = "/dev/disk/by-label/SWAP"; }
+    ];
+
     # fileSystems =
     #     (pkgs.lib.genAttrs
     #         (
@@ -90,11 +95,6 @@
     #             fsType = "vfat";
     #         }
     #     ];
-
-  swapDevices =
-    [
-        { device = "/dev/disk/by-label/SWAP"; }
-    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
