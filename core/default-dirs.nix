7ea2ff75b82c13aa56502c242f08f194eb80bef7
Brian Merchant <bzm3r@proton.me>
2023-11-05 15:06:42 -0800

    no need for $HOME

--- core/default-dirs.nix
+++ core/default-dirs.nix
@@ -6,18 +6,12 @@
   ];
 
   environment.etc."xdg/user-dirs.defaults".text = ''
-    CONFIG_DIRS=$HOME/config-sys
-    CONFIG_HOME=$HOME/config
-    CACHE_HOME=$HOME/cache
-    DATA_HOME=$HOME/local/share
-    STATE_HOME=$HOME/local/state
-    DESKTOP=$HOME
-    DOWNLOAD=$HOME/downloads
-    TEMPLATES=$HOME/templates
-    PUBLICSHARE=$HOME/public
-    DOCUMENTS=$HOME
-    MUSIC=$HOME/media
-    PICTURES=$HOME/media
-    VIDEOS=$HOME/media
+    CONFIG_DIRS=config-sys
+    CONFIG_HOME=config
+    CACHE_HOME=cache
+    DATA_HOME=local/share
+    STATE_HOME=local/state
+    DESKTOP=
+    DOWNLOAD=downloads
   '';
 }
