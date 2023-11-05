3691b0bec292393c9c9225dddd291b4831d1700c
Brian Merchant <bzm3r@proton.me>
2023-11-05 15:10:46 -0800

    xdg's config dir stuff isn't working as i would expect, for now, just simplify

--- core/default-dirs.nix
+++ core/default-dirs.nix
@@ -6,11 +6,6 @@
   ];
 
   environment.etc."xdg/user-dirs.defaults".text = ''
-    CONFIG_DIRS=config-sys
-    CONFIG_HOME=config
-    CACHE_HOME=cache
-    DATA_HOME=local/share
-    STATE_HOME=local/state
     DESKTOP=
     DOWNLOAD=downloads
   '';
