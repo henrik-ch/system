c6d8ad64e68c6fb40a9b7aa5f24c633db7659ad1
Brian Merchant <bzm3r@proton.me>
2023-11-11 19:19:50 -0800

    trying to get audio to work...no success

--- core/audio.nix
+++ core/audio.nix
@@ -1,19 +1,22 @@
 { pkgs, ... }:
 
 {
-  hardware.pulseaudio.enable = false;
-
+  
   services = {
     # audio
     pipewire = {
       enable = true;
       alsa.enable = true;
-      alsa.support32Bit = true;
       pulse.enable = true;
+     wireplumber = {
+        enable = true;
+      };
     };
     # how to handle alsa-plugins?
     xserver.libinput.enable = true;
   };
+
+  programs.noisetorch.enable = true;
 
   security = {
     # for pulseaudio, pipewire, etc.
