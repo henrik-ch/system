b117473c976611543898e5fed6e6e756e3e6b57d
Brian Merchant <bzm3r@proton.me>
2023-11-15 11:37:50 -0800

    trying to get terminal contrast up, did not really work

--- home-common/.config/wezterm/wezterm.lua
+++ home-common/.config/wezterm/wezterm.lua
@@ -14,8 +14,14 @@ end
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Rasi (terminal.sexy)'
-config.font =  wezterm.font_with_fallback {'Inconsolata Nerd Font Mono', 'Fira Mono'}
+config.color_scheme = 'Mikazuki'
+config.font =  wezterm.font_with_fallback {
+  {
+    family = "Inconsolata Nerd Font Mono",
+    harfbuzz_features = { "calt=1", "clig=1", "liga=1" },
+  },
+  'Fira Mono'
+}
 config.font_size = fontprops.size
 
 -- and finally, return the configuration to wezterm
