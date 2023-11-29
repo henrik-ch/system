716825034d0af435ba34c1ecbd42ca6be8b0969e
Brian Merchant <bzm3r@proton.me>
2023-11-28 23:07:07 -0800

    updates

--- programs/aliases.nix
+++ programs/aliases.nix
@@ -4,20 +4,28 @@
     shellAliases =
     let
       repo_root = "$(git rev-parse --show-toplevel)";
-      _re = "sudo /home/bzm3r/nixos-conf/rebuild switch; exec zsh";
+      cmdBuilder = builtins.concatStringsSep ";";
+      ___re = rebuild_opts: cmdBuilder [
+        "sudo ~bzm3r/nixos-conf/rebuild switch${rebuild_opts}"
+        "exec zsh"
+      ];
+      __re = ___re " --show-trace";
+      _re = ___re "";
+      _up = cmdBuilder [
+        "npins -d ~bzm3r/nixos-conf/npins/ update"
+        "${_re}"
+      ];
     in
     {
-      sy = "sway";
+      wm = "sway";
       _conf = "hx ~/nixos-conf";
       _core = "hx ~/nixos-conf/core/";
       _usr = "hx ~/nixos-conf/programs/";
       _wez = "hx ~/nixos-conf/home-common/.config/wezterm";
       _hx = "hx ~/nixos-conf/home-common/.config/helix/config.toml";
       _sy = "hx ~/nixos-conf/home-common/.config/sway";
-      _nsy = "hx ~/nixos-conf/gui-sway.nix";
-      inherit _re;
-      __re = "sudo /home/bzm3r/nixos-conf/rebuild --show-trace switch; exec zsh";
-      _up = "cd /home/bzm3r/nixos-conf ; npins update ; ${_re} ; exec zsh";
+      _nsy = "hx ~/nixos-conf/programs/gui.nix";
+      inherit _re __re _up;
       gpg-import = "gpg --import-options restore --import";
       _wipe = "sudo nix-collect-garbage -d";
       _reboot = "sudo reboot -h now";
@@ -25,8 +33,12 @@
       ls = "lsd -a";
       cat = "bat";
       gs = "ls ; git status";
-      gc = "git commit --message=";
-      "gc.a" = "git commit --amend";
+      gc = "git commit ";
+      ugc = "git reset HEAD~";
+      "gc." = "git commit -c $(git log -n 1 --format='%H')";
+      gcm = "git commit -m ";
+      "gc.a" = "git commit --amend --no-edit";
+      "gc.a." = "git commit --amend";
       gf = "git fetch";
       gm = "git merge";
       gpull = "git pull ; git submodule update --init --recursive";
