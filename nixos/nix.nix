d93f77a1cd40baf70a39a127851c9c40ac877ee9
Brian Merchant <bzm3r@proton.me>
2023-12-01 16:30:41 -0800

    not interested in direnv

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -1,10 +1,4 @@
 { pkgs, ... }: {
-  programs.direnv = {
-    enable = true;
-    loadInNixShell = true;
-    nix-direnv.enable = true;
-  };
-
   environment.systemPackages = with pkgs; [
     nil
     nixpkgs-fmt
