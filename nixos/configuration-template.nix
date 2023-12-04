1a8c88b60db9106599aac22578ffae55933a23b2
Brian Merchant <bzm3r@proton.me>
2023-12-04 00:17:42 -0800

    Remove some stuff from back when we were using misterio's config as a template; update nixpkgs sources.

--- nixos/configuration-template.nix
+++ nixos/configuration-template.nix
@@ -25,28 +25,12 @@ inputs@{ lib
   [
     ./gui
   ];
-
-  # This will add each flake input as a registry
-  # To make nix3 commands consistent with your flake
-  nix.registry = (lib.mapAttrs (_: flake: { inherit flake; })) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
-
-  # This will additionally add your inputs to the system's legacy channels
-  # Making legacy nix commands consistent as well, awesome!
-  nix.nixPath = [ "/etc/nix/path" ];
-  environment.etc =
-    lib.mapAttrs'
-      (name: value: {
-        name = "nix/path/${name}";
-        value.source = value.flake;
-      })
-      config.nix.registry;
-
-  # enable some of the experimental commands for now
-  nix.settings.experimental-features = [ "nix-command" ];
-
   nix.settings = {
     # Deduplicate and optimize nix store
     auto-optimise-store = true;
+    # enable some of the experimental commands for now
+    experimental-features = [ "nix-command" ];
+
   };
 
   hardware.opengl.enable = true;
