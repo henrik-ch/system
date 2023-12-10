b716ea737388927aa7386bce1d19c7afbf421328
Brian Merchant <bzm3r@proton.me>
2023-12-10 00:28:25 -0800

    remove echos, and use the set -x flag instead

--- min-iso/burn-iso.sh
+++ min-iso/burn-iso.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -euxo pipefail
 
 host=$(hostname)
-echo "Copying iso to ${1}..."
 sudo cp ./result-${host}/iso/*.iso ${1}
-echo "Syncing..."
 sync
-echo "Finished copying to USB at ${1}!"
--- min-iso/gen-iso-template.sh
+++ min-iso/gen-iso-template.sh
@@ -1,15 +1,7 @@
-#!/bin/sh
+#/usr/bin/env bash
+set -euxo pipefail
+
 host=$(hostname)
 rm -rf result-${host}
-if  [$? -eq 0 ]; then
-    echo "Successfully removed existing result-${host}"
-else
-    echo "Could note delete result-${host}"
-fi
 
 nix-shell -p nixos-generators --run "nixos-generate --format iso --configuration ./minimal.nix -o result-${host}"
-if  [$? -eq 0 ]; then
-    echo "Finished creating iso at result-${host}"
-else
-    echo "Error creating iso at result-${host}"
-fi
